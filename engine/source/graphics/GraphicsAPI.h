e4a93c2a2967a8ce1d508788f6990faa35aeaa82
GuilhermeAlvesTeixeira <guilhermealvesteixeirasilva@gmail.com>
2026-05-20 21:34:28 -0300

    Add Mesh class, VertexLayout and update Graphics API to CreateVertexBuffer and CreateIndexBuffer

--- engine/source/graphics/GraphicsAPI.h
+++ engine/source/graphics/GraphicsAPI.h
@@ -2,6 +2,7 @@
 #include <glad/glad.h>
 #include <memory>
 #include <string>
+#include <vector>
 
 // The graphics API centralizes rendering operations.
 
@@ -15,6 +16,9 @@ namespace eng {
             std::shared_ptr<ShaderProgram> CreateShaderProgram(const std::string& vertexSource, 
                                                                const std::string& fragmentSource);
         
+            GLuint CreateVertexBuffer(const std::vector<float>& vertices);
+            GLuint CreateIndexBuffer(const std::vector<uint32_t>& indices);
+
             void BindShaderProgram(ShaderProgram* shaderProgram);
             void BindMaterial(Material* material);
     };
